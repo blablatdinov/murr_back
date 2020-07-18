6a9981a38fdcb60a040d919c15fa28c3cc67785b
almaz <a.ilaletdinov@yandex.ru>
2020-07-18 19:03:05 +0000

    add likes in admin panel

--- murr_card/admin.py
+++ murr_card/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import MurrCard, Comment
+from .models import MurrCard, Comment, Like
+
 
 admin.site.register(MurrCard)
 admin.site.register(Comment)
+admin.site.register(Like)
